7d716567185de3c2e39eb57781180929c4ae44a0
Senestres <100228689+Senestres@users.noreply.github.com>
2023-11-17 10:48:45 +0100

    deploy to docs

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,10 +2,10 @@ module.exports = {
 	title: "Eleventy Base Blog v8",
 	url: "https://example.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "I write things so I can stop thinking them",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
+		name: "Senestres",
+		//email: "youremailaddress@example.com",
 		url: "https://example.com/about-me/"
 	}
 }
