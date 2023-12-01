f8ffece84aba9c94cabfa31594c036f50f990d3a
Senestres <100228689+Senestres@users.noreply.github.com>
2023-12-01 10:45:55 +0100

    sitemap

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
 	title: "Traces blog",
-	url: "https://senestres.github.io/traces/",
+	url: "https://tracesblog.net/",
 	language: "fr",
 	description: "Un site sur le corps, la culture et l'apprentissage",
 	author: {
 		name: "Yann Daout",
 		//email: "youremailaddress@example.com",
-		url: "https://senestres.github.io/yanndaout/about/"
+		url: "https://tracesblog.net/about/"
 	}
 }
