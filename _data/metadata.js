3b9191b7d8d37d08635cf8110a81b3536e03c932
Senestres <100228689+Senestres@users.noreply.github.com>
2024-07-31 23:23:19 +0200

    fix site description

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,7 +2,7 @@ module.exports = {
 	title: "Traces",
 	url: "https://tracesblog.net/",
 	language: "fr",
-	description: "Un site sur le corps, la culture et l'apprentissage",
+	description: "Traces est un site sur le corps, la culture et l'apprentissage, écrit par Yann Daout",
 	author: {
 		name: "Yann Daout",
 		//email: "youremailaddress@example.com",
