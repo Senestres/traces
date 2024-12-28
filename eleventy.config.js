42fbe02983109faa713144c8a5d01e78e93de44f
Senestres <100228689+Senestres@users.noreply.github.com>
2024-12-28 15:00:25 +0100

    replace quotes

--- eleventy.config.js
+++ eleventy.config.js
@@ -172,7 +172,8 @@ export default function (eleventyConfig) {
 	// add excerpt
 	// use with <p>{{ post.templateContent | excerpt }}</p>
 	eleventyConfig.addFilter("excerpt", (post) => {
-		const content = post.replace(/(<([^>]+)>)|#|&quot;/gi, "");
+		var content = post.replace(/(<([^>]+)>)|#/gi, "");
+		content = content.replace(/&quot;/gi, "'");
 		return content.substr(0, content.lastIndexOf(" ", 250)) + "...";
 	  });
 
