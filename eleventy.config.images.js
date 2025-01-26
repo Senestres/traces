b9a55a4269603bfd08b8e0b74af7dec95be10edb
Senestres <100228689+Senestres@users.noreply.github.com>
2025-01-26 10:12:32 +0100

    center article img

--- eleventy.config.images.js
+++ eleventy.config.images.js
@@ -23,7 +23,6 @@ export default function (eleventyConfig) {
 			outputDir: path.join(eleventyConfig.dir.output, "img"), // Advanced usage note: `eleventyConfig.dir` works here because we’re using addPlugin.
 		});
 
-		// TODO loading=eager and fetchpriority=high
 		let imageAttributes = {
 			alt,
 			sizes,
