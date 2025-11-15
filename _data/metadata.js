5a5b3535533a90ae816d89e4e92742e603956ee4
Senestres <100228689+Senestres@users.noreply.github.com>
2025-11-15 12:14:20 +0100

    fix base url typo

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,7 +1,7 @@
 export default {
 	title: "Traces",
 	url: "https://traces.info/",
-	base: "https://traces.info/",
+	base: "https://traces.info",
 	language: "fr",
 	description: "Traces est un site sur le corps, la culture et l'apprentissage",
 	author: {
